d6aa5e9a73817ea295e158d4f71bfb5b3af801c3
rilkeye <349151990@qq.com>
2019-02-18 17:19:21 +0800

    Add files via upload
    version 1.0

---
+++ main.py
@@ -0,0 +1,33 @@
+import utils
+import lightgbm as lgb
+
+'''
+载入train_dataset.csv、test_dataset.csv文件，构建二维数组
+缺省train、test首列：“用户编码”；train末列：“信用分”
+'''
+train_dataset_path = 'data/train_dataset.csv'
+test_dataset_path = 'data/test_dataset.csv'
+
+# data_array: shape = (50000,28); score: shape = (50000,)
+train_data_array, score = utils.build_data_array(train_dataset_path, tag='train')
+test_data_array = utils.build_data_array(test_dataset_path, tag='test')
+
+
+# 将numpy数组加载到数据集中
+train_data = lgb.Dataset(train_data_array, label=score)
+train_data.save_binary('data/train.bin')
+
+# Parameters setting
+num_round = 1
+param = { 'metric':'l1'}
+
+# Train & save model as model.txt
+bst = lgb.train(params=param, train_set=train_data, num_boost_round=num_round, valid_sets=None)
+bst.save_model('model/model.txt')
+
+# Load model and make prediction
+bst = lgb.Booster(model_file='model/model.txt')
+pred = bst.predict(test_data_array)
+
+# 将结果按赛制要求写入文件
+utils.write_SubmitionFile(pred, test_dataset_path)
---
+++ utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import csv
+
+def build_data_array(path, tag):
+    train_data = []
+    with open(path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            train_data.append(line)
+
+    del (train_data[0])  # 删除.csv文件首行无关信息
+
+    array = np.zeros(shape=(50000, 28))
+    score = np.zeros(shape=(50000,))
+
+    if tag == 'test':
+        for row_idx in range(50000):
+            for col_idx in range(1, 29):
+                array[row_idx][col_idx - 1] = float(train_data[row_idx][col_idx])
+        return array
+
+    if tag == 'train':
+        for row_idx in range(50000):
+            for col_idx in range(1, 29):
+                array[row_idx][col_idx - 1] = float(train_data[row_idx][col_idx])
+            score[row_idx] = float(train_data[row_idx][28])
+        return array, score
+    else:
+        raise()
+
+def write_SubmitionFile(result, id_path):
+    score = result
+    with open(id_path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        id = [row[0] for row in reader]
+    del id[0]
+
+    dataframe = pd.DataFrame({'id':id, 'score':score})
+    dataframe.to_csv('data/submit.csv',index=False, sep=',')
+    print('Submitfile had saved...')
