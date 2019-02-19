0337085336b4f5848e0346d91d5bde0c52f2186d
rilkeye <349151990@qq.com>
2019-02-19 11:39:35 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author : Rilke
+
 import utils
 import lightgbm as lgb
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 
 '''
 载入train_dataset.csv、test_dataset.csv文件，构建二维数组
@@ -9,21 +15,30 @@ train_dataset_path = 'data/train_dataset.csv'
 test_dataset_path = 'data/test_dataset.csv'
 
 # data_array: shape = (50000,28); score: shape = (50000,)
-train_data_array, score = utils.build_data_array(train_dataset_path, tag='train')
-test_data_array = utils.build_data_array(test_dataset_path, tag='test')
+train_data_array = utils.build_data_array(train_dataset_path, tag='train')
+test_data_array  = utils.build_data_array(test_dataset_path, tag='test')
+
+# 拆分数据集 & 分离data、label
+train_dataset, validation = train_test_split(train_data_array, test_size=0.2, random_state=21)
+train_dataset, train_label = utils.split_data_and_label(train_dataset)
+valid_dataset, valid_label = utils.split_data_and_label(validation)
 
 
-# 将numpy数组加载到数据集中
-train_data = lgb.Dataset(train_data_array, label=score)
+# 训练集加载
+train_data = lgb.Dataset(train_dataset, label=train_label)
 train_data.save_binary('data/train.bin')
 
+# 验证集加载
+valid_data = lgb.Dataset(valid_dataset, label=valid_label, reference=train_data)
+valid_data.save_binary('data/valid.bin')
+
 # Parameters setting
-num_round = 1
-param = {'objective':'regression'}
-param['metric'] = 'auc'
+num_round = 1000
+params = {'objective':'regression', 'metric':'auc'}
+
 
 # Train & save model as model.txt
-bst = lgb.train(params=param, train_set=train_data, num_boost_round=num_round, valid_sets=None)
+bst = lgb.train(params=params, train_set=train_data, num_boost_round=num_round, valid_sets=None)
 bst.save_model('model/model.txt')
 
 # Load model and make prediction
@@ -33,3 +48,4 @@ pred = [int(round(score)) for score in pred] # 将pred中的float型四舍五入
 
 # 将结果按赛制要求写入文件
 utils.write_SubmitionFile(pred, test_dataset_path)
+print(pred[:100])
--- utils.py
+++ utils.py
@@ -1,33 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author : Rilke
+
 import numpy as np
 import pandas as pd
 import csv
 
 def build_data_array(path, tag):
+    '''
+
+    :param path: file path
+    :param tag: "train" or "test"
+    :return: test array : (50000, 28) ; train array : (50000, 29)
+    '''
     train_data = []
     with open(path, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         for line in reader:
             train_data.append(line)
 
-    del (train_data[0])  # 删除.csv文件首行无关信息
-
-    array = np.zeros(shape=(50000, 28))
-    score = np.zeros(shape=(50000,))
+    del (train_data[0])  # 删除.csv文件首行id信息
 
     if tag == 'test':
+        array = np.zeros(shape=(50000,28))
         for row_idx in range(50000):
             for col_idx in range(1, 29):
                 array[row_idx][col_idx - 1] = float(train_data[row_idx][col_idx])
         return array
 
     if tag == 'train':
+        array = np.zeros(shape=(50000,29))
         for row_idx in range(50000):
-            for col_idx in range(1, 29):
+            for col_idx in range(1, 30):
                 array[row_idx][col_idx - 1] = float(train_data[row_idx][col_idx])
-            score[row_idx] = float(train_data[row_idx][29])
-        return array, score
+        return array
     else:
         raise()
+
+def split_data_and_label(array):
+    label = array[:,-1] # 取array最后一列数据
+    array = np.delete(array, -1, axis=1) # del last column of array
+    return array, label
 
 def write_SubmitionFile(result, id_path):
     score = result
