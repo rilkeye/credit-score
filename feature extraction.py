01126ac941eed17ccbfdb6c8a30aa2a3f45568eb
rilkeye <349151990@qq.com>
2019-02-21 11:45:32 +0800

    Add files via upload

--- feature extraction.py
+++ feature extraction.py
@@ -6,6 +6,9 @@ import utils
 
 import pandas as pd
 import numpy as np
+import math
+import csv
+from sklearn import metrics as mr
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
@@ -13,9 +16,90 @@ from sklearn.model_selection import cross_val_score
 import pylab as mpl
 
 
-def rmse_cv(model):
-    rmse= np.sqrt(-cross_val_score(model, X_train, y, scoring="neg_mean_squared_error", cv = 3))
-    return(rmse)
+# 计算特征和类的平均值
+def calcMean(x,y):
+    sum_x = sum(x)
+    sum_y = sum(y)
+    n = len(x)
+    x_mean = float(sum_x+0.0)/n
+    y_mean = float(sum_y+0.0)/n
+    return x_mean,y_mean
+
+# 计算Pearson系数
+def cal_Pearson(x,y):
+    x_mean,y_mean = calcMean(x,y) # 计算x,y向量平均值
+    n = len(x)
+    sumTop = 0.0
+    sumBottom = 0.0
+    x_pow = 0.0
+    y_pow = 0.0
+    for i in range(n):
+        sumTop += (x[i]-x_mean)*(y[i]-y_mean)
+    for i in range(n):
+        x_pow += math.pow(x[i]-x_mean,2)
+    for i in range(n):
+        y_pow += math.pow(y[i]-y_mean,2)
+    sumBottom = math.sqrt(x_pow*y_pow)
+    p = sumTop/sumBottom
+    return p
+
+def get_Pearson(array):
+    '''
+    :return: Pearson dict
+    '''
+    Pearson = {}
+    score = utils.get_array_column(array, -1)
+    # 获取特征标签
+    with open('data/train_dataset.csv', 'r', encoding='utf-8-sig') as f:
+        reader = csv.reader(f)
+        for idx, line in enumerate(reader):
+            if idx == 0:
+                firstline = line  # 首行，特征标签行
+
+    del firstline[-1] # 删除“信用分”
+    del firstline[0] # 删除“用户编码”
+
+    # 计算特征Pearson相关系数
+    for i in range(array.shape[1] - 1):
+        feature = utils.get_array_column(array, i)
+        pearson_i = cal_Pearson(feature, score)
+        Pearson[str(firstline[i])] = pearson_i
+        if i == 14:
+            print(cal_Pearson(feature, utils.get_array_column(array, i+1)))
+    A = np.zeros(50000,)
+    for No in range(21,28):
+        fea = utils.get_array_column(array, i)
+        A += fea
+
+    print(cal_Pearson(A, score))
+
+    return Pearson
+
+        # with open('data/pearson.txt', 'a', encoding='utf-8') as f:
+        #    f.write('Pearson correlation of feature {} "{}" is : {}'.format(i, firstline[i], pearson_i))
+        #    f.write('\n')
+
+def get_MI(array):
+    '''
+    计算信息增益MI
+    :param array: 最后一列为label或score的矩阵
+    '''
+    score = utils.get_array_column(array, -1)
+    # 获取特征标签
+    with open('data/train_dataset.csv', 'r', encoding='utf-8-sig') as f:
+        reader = csv.reader(f)
+        for idx, line in enumerate(reader):
+            if idx == 0:
+                firstline = line  # 首行，特征标签行
+
+    del firstline[-1]  # 删除“信用分”
+    del firstline[0]  # 删除“用户编码”
+
+    for i in range(array.shape[1] - 1):
+        feature = utils.get_array_column(array, i)
+        MI = mr.mutual_info_score(feature, score)
+        print('MI of feature {} "{}" is : {}'.format(i, firstline[i], MI))
+    return MI
 
 def LassoCV():
     train = pd.read_csv('data/train_dataset.csv', header=0)  # Load the train file into a dataframe
@@ -40,7 +124,7 @@ def LassoCV():
         sum(coef == 0)) + " variables")
 
     # 输出所选择的最优正则化参数情况下的残差平均值，因为是3折，所以看平均值
-    print(rmse_cv(model_lasso).mean())
+    print(np.sqrt(-cross_val_score(model_lasso, X_train, y, scoring="neg_mean_squared_error", cv = 3)).mean())
 
     # 画出特征变量的重要程度，这里面选出前17个重要，后10个不重要的举例
     imp_coef = pd.concat([coef.sort_values().head(17),
